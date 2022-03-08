be9f3a889d03e92577a865fea3a92be90825b658
iamHrithikRaj <iamHrithikRaj26@gmail.com>
2022-03-08 20:40:47 +0530

    connected api

--- src/components/QuestionHeader.js
+++ src/components/QuestionHeader.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import classes from './QuestionHeader.module.css';
-import DiamondOutlinedIcon from '@mui/icons-material/DiamondOutlined';
+import React from "react";
+import classes from "./QuestionHeader.module.css";
+import DiamondOutlinedIcon from "@mui/icons-material/DiamondOutlined";
 
 const QuestionHeader = (props) => {
   return (
     <div className={classes.container}>
       <div className={classes.innercontainer}>
-        <div>#{props.sno}</div>
+        <div>{props.sno}</div>
         <div>{props.title}</div>
       </div>
       <div className={classes.innercontainer}>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,16 +2,36 @@ import React, { useRef } from 'react';
 import classes from './Login.module.css';
 import amitylogo from '../images/amitylogo.png';
 import cyberlogo from '../images/cyberlogo.png';
+import axios from 'axios';
+import { Routes, Route, Navigate } from "react-router-dom";
+
 
 const Login = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+  const baseURL = "https://pure-brook-94362.herokuapp.com/api/v1/team"
 
   const submitHandler = (e) => {
     e.preventDefault();
     console.log(emailInputRef.current.value);
     console.log(passwordInputRef.current.value);
+    const data = {
+      "teamName": emailInputRef.current.value,
+      "password": passwordInputRef.current.value
+    }
+    console.log(data);
+    axios.post(`${baseURL}/login`, data)
+    .then(function (response) {
+      console.log(response);
+      window.location.href="/game"
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  
   };
+
+  
 
   return (
     <div className={classes.background}>
@@ -23,7 +43,6 @@ const Login = () => {
               src={amitylogo}
               alt='Amity Logo'
             />
-
             <img
               className={classes.cyberlogo}
               src={cyberlogo}
